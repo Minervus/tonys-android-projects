738baa5217c168fb4ebc4c8d7406087499d39615
Minervus <tnguyen411@gmail.com>
2023-10-10 11:15:48 -0700

    first commit

--- app/src/main/java/com/tonys/tonysandroidprojects/MainActivity.java
+++ app/src/main/java/com/tonys/tonysandroidprojects/MainActivity.java
@@ -10,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Project[] projects = {
+                new Project("Getting Started","Very first app for getting started", R.drawable.getting_started),
+                new Project("Tony Nguyen App","App to display some text in textView", R.drawable.quote),
+                new Project("BMI Calculator","A app to display a user's BMI", R.drawable.tape),
+                new Project("1RM Calculator","An app to calculate a users estimated 1 rep max", R.drawable.calculator),
+                new Project("The Hungry Dev","A restaurant app displaying a menu", R.drawable.hungry_developer)
+        };
     }
 }
---
+++ app/src/main/java/com/tonys/tonysandroidprojects/Project.java
@@ -0,0 +1,15 @@
+package com.tonys.tonysandroidprojects;
+
+public class Project {
+    // Classes have 3 things: Fields, Methods, and Constructor
+
+    String name;
+    String description;
+    int image;
+
+    Project(String name, String description, int image){
+        this.name = name;
+        this.description = description;
+        this.image = image;
+    }
+}
